26b79532ca11e7c9a58c9b8cc04081610cde849b
Yannick Kahl <yannick.kahl@epitech.eu>
2026-04-07 14:37:04 +0200

    [+] executable memory allocation done

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+use libc;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Too few arguments");
+        std::process::exit(1);
+    }
+    let data: Vec<u8> = fs::read(&args[1]).expect("Failed to read file");
+    let mem = unsafe {
+        libc::mmap(std::ptr::null_mut(), data.len(), libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC,
+        libc::MAP_PRIVATE | libc::MAP_ANON, -1, 0)
+    };
 }
