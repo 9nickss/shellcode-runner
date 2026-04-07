afdc95412c3ca7bd9f09a916e4755481152a0112
Yannick Kahl <yannick.kahl@epitech.eu>
2026-04-07 16:13:30 +0200

    [~] refactor code

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -2,21 +2,38 @@ use std::env;
 use std::fs;
 use libc;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn read_shellcode(path: &str) -> Vec<u8> {
+    let data: Vec<u8> = fs::read(path).expect("Failed to read file");
+    return data;
+}
 
-    if args.len() < 2 {
-        eprintln!("Too few arguments");
-        std::process::exit(1);
-    }
-    let data: Vec<u8> = fs::read(&args[1]).expect("Failed to read file");
+fn alloc_executable_memory(size: usize) -> *mut libc::c_void {
     unsafe {
-        let mem = libc::mmap(std::ptr::null_mut(), data.len(), libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC,
+        let mem = libc::mmap(std::ptr::null_mut(), size, libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC,
         libc::MAP_PRIVATE | libc::MAP_ANON, -1, 0);
         if mem == libc::MAP_FAILED {
             eprintln!("mmap failed");
             std::process::exit(1);
         }
-        std::ptr::copy_nonoverlapping(data.as_ptr(), mem as *mut u8, data.len());
+        return mem;
     };
+}
+
+fn copy_to_mem(shellcode: &[u8], mem: *mut libc::c_void) {
+    unsafe {
+        std::ptr::copy_nonoverlapping(shellcode.as_ptr(), mem as *mut u8, shellcode.len());
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Too few arguments");
+        std::process::exit(1);
+    }
+    let shellcode = read_shellcode(&args[1]);
+    let size = shellcode.len();
+    let mem = alloc_executable_memory(size);
+    copy_to_mem(&shellcode, mem);
+    
 }
