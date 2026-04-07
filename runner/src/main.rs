8fd93471fd7724accc41c5e0bc1be31f957483fd
Yannick Kahl <yannick.kahl@epitech.eu>
2026-04-07 14:42:40 +0200

    [+] copy code in mem and mmap fail check

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -10,8 +10,13 @@ fn main() {
         std::process::exit(1);
     }
     let data: Vec<u8> = fs::read(&args[1]).expect("Failed to read file");
-    let mem = unsafe {
-        libc::mmap(std::ptr::null_mut(), data.len(), libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC,
-        libc::MAP_PRIVATE | libc::MAP_ANON, -1, 0)
+    unsafe {
+        let mem = libc::mmap(std::ptr::null_mut(), data.len(), libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC,
+        libc::MAP_PRIVATE | libc::MAP_ANON, -1, 0);
+        if mem == libc::MAP_FAILED {
+            eprintln!("mmap failed");
+            std::process::exit(1);
+        }
+        std::ptr::copy_nonoverlapping(data.as_ptr(), mem as *mut u8, data.len());
     };
 }
