649300a5c3d88bc7be9667fb7021d4ba64e1818d
Yannick Kahl <yannick.kahl@epitech.eu>
2026-04-09 00:06:57 +0200

    [+] added key parsing for encryptor

--- runner/src/encryptor.rs
+++ runner/src/encryptor.rs
@@ -1,15 +1,28 @@
 use std::env;
+use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn check_file(file: &str) -> Vec<u8> {
+    fs::read(file).expect("Failed to read file")
+}
 
+fn parse_key(key: &str) -> u8 {
+    u8::from_str_radix(key.strip_prefix("0x").expect("Invalid key prefix"), 16)
+        .expect("Invalid hex value in key") // 16 car hexadecimal (XOR)
+}
+
+fn check_args_key(args: &Vec<String>) -> u8 {
     if args.len() < 2 || args.len() > 3 { // avec key custom ou key de base
         eprintln!("Wrong number of arguments");
         std::process::exit(1);
     }
     if args.len() == 3 {
-        let key = args[2];
+        return parse_key(&args[2]);
     }
-    let key = 0xAA;
-    
+    0xAA
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let key = check_args_key(&args);
+    check_file(&args[1]);
 }
