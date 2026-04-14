f273809f4a7eea28db168cd77a5e992726180279
Yannick Kahl <yannick.kahl@epitech.eu>
2026-04-14 12:46:52 +0200

    [~] updated lib architecture

---
+++ shellcode_runner/lib/src/crypt.rs
@@ -0,0 +1,11 @@
+pub fn xor_crypt(code: &mut Vec<u8>, key: u8){
+        for byte in code.iter_mut() {
+            *byte ^= key;
+        }
+    }
+
+pub fn parse_hex(s: &str) -> Result<u8, String> {
+    let cleaned = s.trim_start_matches("0x").trim_start_matches("0X");
+    u8::from_str_radix(cleaned, 16)
+        .map_err(|_| format!("Invalid hex value: {}", s))
+}
